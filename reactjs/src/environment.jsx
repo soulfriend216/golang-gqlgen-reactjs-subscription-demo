a2bfaaefb0afd2e8741cef31669fd2b796f35d3c
$oulFr!end <soulfriend216@gmail.com>
2022-10-01 02:30:47 +0900

    Update environment.jsx
    Co-authored-by: devalpha18 <devalpha18@gmail.com>

--- reactjs/src/environment.jsx
+++ reactjs/src/environment.jsx
@@ -1,7 +1,7 @@
 
 // export const Host = "192.168.0.2"
 export const Host = "localhost"
-export const Port = "8888";
+export const Port = "5000";
 
 export const BASE_API = "http://"+Host+":"+Port+"/";
 export const WSHost = "ws://"+Host+":"+Port+"/query";
